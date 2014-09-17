0f86f2c407155e555dd3ddc831911608d947e3ea
krdlab <krdlab@gmail.com>
2014-09-17 17:05:53 +0900

    [try-hubot] Add test server for a practice of hubot adapter

---
+++ try-hubot/test-server/src/Memo/Model.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE DeriveGeneric     #-}
+module Memo.Model where
+
+import Data.Text (Text)
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics
+import Data.IntMap
+import Prelude hiding (id)
+
+data Memo = Memo { name :: Text, content :: Text } deriving (Show, Generic)
+data StoredMemo = StoredMemo { id :: Int, memo :: Memo } deriving (Show, Generic)
+
+instance FromJSON Memo
+instance ToJSON   Memo
+
+instance ToJSON   StoredMemo
+
+data MemoryStore = MemoryStore { nextId :: Int, records :: IntMap StoredMemo }
+
+emptyStore :: MemoryStore
+emptyStore = MemoryStore 1 empty
+
