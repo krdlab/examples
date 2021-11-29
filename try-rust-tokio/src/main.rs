954b41f98f9ba020250edd3c4bb590febbaddaa2
krdlab <krdlab@gmail.com>
2021-11-30 00:37:44 +0900

    feat: [try-rust-tokio] set and get a value

--- try-rust-tokio/src/main.rs
+++ try-rust-tokio/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+// Copyright (c) 2021 Sho Kuroda <krdlab@gmail.com>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    client.set("hello", "world".into()).await?;
+
+    let result = client.get("hello").await?;
+
+    println!("result = {:?}", result);
+
+    Ok(())
 }
