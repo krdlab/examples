ae184ff7cb9ccf49d3f1dad0ccfc54d84e278188
krdlab <krdlab@gmail.com>
2021-12-01 21:52:32 +0900

    feat: [try-rust-tokio] accept sockets

--- try-rust-tokio/src/main.rs
+++ try-rust-tokio/src/main.rs
@@ -3,6 +3,24 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-fn main() {
-    println!("main");
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
+
+async fn process(socket: TcpStream) {
+    let mut conn = Connection::new(socket);
+    if let Some(frame) = conn.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
+        let response = Frame::Error("unimplemented".to_string());
+        conn.write_frame(&response).await.unwrap();
+    }
 }
