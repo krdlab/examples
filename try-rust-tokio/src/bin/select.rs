e0269436a8b4e7122e691a31a489d3daef5630ed
krdlab <krdlab@gmail.com>
2021-12-31 20:10:35 +0900

    feat: [try-rust-tokio] use tokio::select!

---
+++ try-rust-tokio/src/bin/select.rs
@@ -0,0 +1,23 @@
+use tokio::sync::oneshot;
+
+#[tokio::main]
+async fn main() {
+    let (tx1, rx1) = oneshot::channel();
+    let (tx2, rx2) = oneshot::channel();
+
+    tokio::spawn(async {
+        let _ = tx1.send("one");
+    });
+    tokio::spawn(async {
+        let _ = tx2.send("two");
+    });
+
+    tokio::select! {
+      val = rx1 => {
+        println!("{:?}", val);
+      }
+      val = rx2 => {
+        println!("{:?}", val);
+      }
+    }
+}
