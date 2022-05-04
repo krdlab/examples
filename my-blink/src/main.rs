450206c9bafd6c13353b8d07d6a699da33fa41e6
krdlab <krdlab@gmail.com>
2022-05-04 20:41:32 +0900

    feat(my-blink): blink LED

--- my-blink/src/main.rs
+++ my-blink/src/main.rs
@@ -9,6 +9,7 @@ fn main() -> ! {
     let (mut user_led, mut delay) = support::init();
 
     loop {
-        // TODO: ここにLチカのコードを書く
+        delay.delay_ms(500u16);
+        user_led.toggle();
     }
 }
