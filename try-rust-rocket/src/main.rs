6d37f5b7ce8bbd9d19aa6e6847d1dbb2fad3f24c
krdlab <krdlab@gmail.com>
2022-07-19 13:32:32 +0900

    feat: https://rocket.rs/v0.5-rc/guide/getting-started/

--- try-rust-rocket/src/main.rs
+++ try-rust-rocket/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
