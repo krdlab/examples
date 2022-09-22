56964ba1785513b50dbb49bee0c7ae4489ec6093
krdlab <krdlab@gmail.com>
2022-09-23 01:15:46 +0900

    feat(hello_macro): try implementing custom derive macro in Rust

---
+++ hello_macro-in-rust/README.md
@@ -0,0 +1,1 @@
+[How to Write a Custom derive Macro](https://doc.rust-lang.org/book/ch19-06-macros.html#how-to-write-a-custom-derive-macro)
--- hello_macro-in-rust/src/main.rs
+++ hello_macro-in-rust/src/main.rs
@@ -1,3 +1,9 @@
+use hello_macro::HelloMacro;
+use hello_macro_derive::HelloMacro;
+
+#[derive(HelloMacro)]
+struct Pancakes;
+
 fn main() {
-    println!("Hello, world!");
+    Pancakes::hello_macro();
 }
